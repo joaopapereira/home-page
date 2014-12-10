cd30223a123460d81612cfdb4431c2e85341bdfd
João Pereira <joaopapereira@gmail.com>
2014-12-10 09:23:45 +0000

    Added RSPEC and tests for models

---
+++ spec/factories/programming_languages.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :programming_language do
+    
+  end
+
+end
---
+++ spec/factories/repo_languages.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :repo_language, :class => 'RepoLanguages' do
+    
+  end
+
+end
