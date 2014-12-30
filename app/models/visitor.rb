89cd1c1c59661bef88e8850da9f7cff2b40b0945
João Pereira <joaopapereira@gmail.com>
2014-12-30 09:48:40 +0000

    Added restriction of 1 IP entry a day

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,5 +1,8 @@
 require 'geoip'
 class Visitor < ActiveRecord::Base
+    def self.find_today_by_ip_address ip_add
+        where("created_at >= ?", Time.zone.now.beginning_of_day).where("ip like ?", ip_add)
+    end
     def set_ip_address ip_add
         c = GeoIP.new('GeoLiteCity.dat').city(ip_add)
         self.ip = ip_add
